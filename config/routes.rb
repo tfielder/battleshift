a2c68be965e73e372ca26f4744550fcd0ff61eac
Tim Fielder <fiel1139@gmail.com>
2018-10-12 18:46:36 -0600

    welcome controller and user story for register.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: "welcome#index"
+  
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do
---
+++ spec/features/visitor_registers_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'as a guest user' do
+  it 'allows visitor to create a log in' do
+    email = "mike@mike.com"
+    name = "Mike"
+    password = "hamburger1"
+
+    visit '/'
+  # And I click "Register"
+    click_on "Register"
+  # Then I should be on "/register"
+    expect(current_path).to eq('/register')
+  # And when I fill in an email address (required)
+    fill_in "email", with: email
+  # And I fill in name (required)
+    fill_in "name", with: name
+  # And I fill in password and password confirmation (required)
+    fill_in "password", with: password
+    fill_in "password_confirmation", with: password
+  # And I click submit
+    click_on "Submit"
+  # Then I should be redirected to "/dashboard"
+    expect(current_path).to eq("/dashboard")
+  # And I should see a message that says "Logged in as <SOME_NAME>"
+    expect(page).to have_content("Logged in as Mike")
+  # And I should see "This account has not yet been activated. Please check your email."
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
+  end
+end

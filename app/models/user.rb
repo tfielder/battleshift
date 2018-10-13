81e764c7ef85117abed362193465265f51909664
Tim Fielder <fiel1139@gmail.com>
2018-10-13 14:22:23 -0600

    adds activation token and digest to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+attr_accessor :activation_token
+before_create :create_activation_digest
+
 validates :name, presence: true
 validates :email, uniqueness: true, presence: true
 validates_presence_of :password, require: true
 
 has_secure_password
+
+private
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end

bb3026b18fac8b2569c0804259997676a5b61373
Tim Fielder <fiel1139@gmail.com>
2018-10-14 13:29:41 -0600

    rework on user_mailer_spec.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    user = User.create!(name: "C.J. Cregg", email: "a@example.org", password: "1234")
+    user = User.create!(name: "C.J. Cregg", email: "@example.org", password: "1234")
     let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Battleshift Account Activation")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq(["@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
 

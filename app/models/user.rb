2bc67bb304d7672ef2b2515a1b35bae28a440736
Tim Fielder <fiel1139@gmail.com>
2018-10-14 19:27:47 -0600

    adds identity token to user model implementation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 attr_accessor :activation_token
+attr_accessor :identity_token
 before_create :create_activation_digest
+before_create :create_identity_digest
 before_save :downcase_email
 
 validates :name, presence: true
@@ -32,7 +34,12 @@ end
 def activate
    update_attribute(:activated,    true)
    update_attribute(:activated_at, Time.zone.now)
- end
+end
+
+def create_identity_digest
+  self.identity_token = User.new_token
+  self.identity_digest = User.digest(identity_token)
+end
 
 private
   def create_activation_digest
---
+++ spec/features/user_creates_a_game_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'as an activated user' do
+  it 'allows a user to create a game with valid email and API key' do
+    #create 2 users and activate
+    #player_1 = User.create(name: , email: , password: , activated: true, api_key: )
+    player_1.
+
+    #player_2 =
+    #visit '/dashboard'
+    #click_on "create game"
+    #expect(current_path).to eq('game/new')
+    #fill_in "API key", with: player_1.api_key
+    #fill_in "Player_2 email", with: player_2.email
+    #expect(page).to have_content("Opponent's email:")
+    #expect(page).to have_content("Your API key")
+  end
+end

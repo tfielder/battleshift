5a968eb4fc7820d55aee33d6992ecefd5788eaf9
Tim Fielder <fiel1139@gmail.com>
2018-10-15 10:16:00 -0600

    adds implementation on welcome page for logging in .

---
+++ spec/features/user_creates_a_game_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'as an activated user' do
+  it 'allows a user to create a game with valid email and API key' do
+    #create 2 users and activate
+    player_1 = User.create(name: "Samwise", email: "littlehouse@theshire.com", password: "mr.frodo!", activated: true)
+    player_2 = User.create(name: "Frodo", email: "mrunderhill@theshire.com", password: "my!preciouse", activated: true)
+
+    email = "littlehouse@theshire.com"
+    password = "mr.frodo!"
+
+    visit '/'
+    fill_in "user[email]", with: email
+    fill_in "user[password]", with: password
+    click_on "Login"
+    #click_on "create game"a
+    click_on "Create Game"
+    #expect(current_path).to eq('game/new')
+    expect(current_path).to eq('game/new')
+    #fill_in "API key", with: player_1.api_key
+    fill_in "API key", with: player_1.identity_token
+    #fill_in "Player_2 email", with: player_2.email
+    fill_in "Player_2 email", with: player_2.email
+    #expect(page).to have_content("Opponent's email:")
+    expect(page).to have_content("Opponent's email:")
+    #expect(page).to have_content("Your API key")
+    expect(page).to have_content("Your API key (check your email)")
+  end
+end

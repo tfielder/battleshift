47e71ad781a9976388feb1a5fe938c2ce0468315
Tim Fielder <fiel1139@gmail.com>
2018-10-15 23:30:22 -0600

    comment out test.

--- spec/requests/posts_a_game_via_api_spec.rb
+++ spec/requests/posts_a_game_via_api_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe 'user can start a game' do
   xit 'posts a game through the api' do
     #create users
-    #user_1 = create()
+    user_1 = User.create(name: "a", email: "b", password: "c", activated: true)
+    user_2 = User.create(name: "d", email: "e", password: "f", activated: true)
     #create game API token
+    #game = Game.create
 
-    post '/api/v1/games' #params?
+    post "/api/v1/games/#{game.id}" #params?
     #game created
     expect(response).to be_successful
   end

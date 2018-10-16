c03baa99a50dfac2950b7c07fee557f6dcc60334
Tim Fielder <fiel1139@gmail.com>
2018-10-16 15:52:09 -0600

    stopping point.

--- spec/requests/posts_a_game_via_api_spec.rb
+++ spec/requests/posts_a_game_via_api_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe 'user can start a game' do
-  xit 'posts a game through the api' do
+  it 'posts a game through the api' do
     #create users
     user_1 = User.create(name: "a", email: "b", password: "c", activated: true)
     user_2 = User.create(name: "d", email: "e", password: "f", activated: true)
     #create game API token
-    #game = Game.create
+    game = Game.create
 
     post "/api/v1/games/#{game.id}" #params?
     #game created

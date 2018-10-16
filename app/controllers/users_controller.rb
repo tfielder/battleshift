0bf59637a4ab0e6ead9caeb84382200ceb8a1e2b
mmbensalah <mmbensalah@gmail.com>
2018-10-16 10:35:19 -0600

    removes binding.pry s and deletes duplicate test user can access user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,8 @@ class UsersController < ApplicationController
     @user = response.fetch_one_user_data
     url.patch("api/v1/users/#{@user[:id]}", {email: params[:email]})
     redirect_to('/users')
+    
+
     flash[:notice] = "Successfully updated #{@user[:name]}"
   end
 
@@ -75,4 +77,3 @@ class UsersController < ApplicationController
     end
 
 end
-

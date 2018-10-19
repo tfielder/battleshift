be9c2b5d0fb34e4d5841454f87037e9677c6b847
Tim Fielder <fiel1139@gmail.com>
2018-10-18 22:06:49 -0600

    dropped down database to bare essentials.

--- db/migrate/20181019040214_add_columns_to_game.rb
+++ db/migrate/20181019040214_add_columns_to_game.rb
@@ -1,4 +1,4 @@
-class AddApIkeystoGame < ActiveRecord::Migration[5.1]
+class AddColumnsToGame < ActiveRecord::Migration[5.1]
   def change
     add_column :games, :player_1_api_key, :string
     add_column :games, :player_2_api_key, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181019020656) do
+ActiveRecord::Schema.define(version: 20181019040214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
